43cd49fdd900407c9eec7c3bb236f11365a58f28
Rita R <prostorita2013.p@yandex.ru>
2023-03-02 22:31:39 +0300

    new commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 fun main() {
     val amount = 1000
     val commission = 0.75
-    val result = (commission * amount)/ 100
+    val minCommissionRub = 35
+    val commissionRub = (commission * amount)/ 100
+    val result = if (commissionRub < minCommissionRub) minCommissionRub else commissionRub
     println("комиссия: $result руб")
 }
